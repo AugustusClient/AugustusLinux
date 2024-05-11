5e99ae03933ee526d7c6f405ae2b9ea6e9af9726
PixGuy <cybercampos.assistencia@gmail.com>
2024-05-11 00:48:21 -0300

    Changed Uninstaller.sh

--- Uninstaller.sh
+++ Uninstaller.sh
@@ -3,9 +3,12 @@
 WHITELISTED_FILES=("LICENSE" "README.md" "Installer.sh" "Uninstaller.sh")
 
 printf "WARNING! THIS SCRIPT WILL REMOVE ALL FILES FROM THIS DIRECTORY ($(pwd)).\nDO YOU WANT TO PROCEED? [y/n]: "
-read warn 
-
-if [[ $(echo "$warn" | awk '{print tolower($0)}') = "y" ]]; then
+read -r warn 
+if [[ ! $warn ]]; then
+  exit 1;
+fi
+warn="${warn,,}"
+if [[ $warn = "y" ]]; then
   for file in *; do 
     if [[ ! "${WHITELISTED_FILES[@]}" =~ "$file" ]]; then
       rm -rf "$file"
